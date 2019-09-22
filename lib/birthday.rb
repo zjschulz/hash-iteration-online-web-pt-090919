1d6cf32d39ed5d912ef50a018d8d4879ebb10ea4
Zachary Schulz <zjschulz@gmail.com>
2019-09-22 16:31:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,6 +15,7 @@ def age_appropriate_birthday(birthday_kids)
     if age <= 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
+    puts "You are too old for this."
     end
   end
 end
