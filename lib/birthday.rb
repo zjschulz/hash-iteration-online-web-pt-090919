d7cba1def3404fd7791da4385bb4ad34fb3526f9
Zachary Schulz <zjschulz@gmail.com>
2019-09-22 16:28:31 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,8 +10,7 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday(hday_kids)
-  
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
