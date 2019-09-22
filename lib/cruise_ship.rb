0a45a311ce400d512f27fbeb868f3bea71e801d9
Zachary Schulz <zjschulz@gmail.com>
2019-09-22 16:23:29 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,8 +8,9 @@
 
 def select_winner(passengers)
   winner = ""
-  passengers.each do |suite,name|
+  passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
+  end
 end
